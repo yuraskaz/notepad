44e8c394d940de96abfe7e125fcce9fff5ea7704
yuraskaz <kazachkoff.yury@yandex.ru>
2016-11-04 00:33:13 +0300

    class Post+ class Memo+ class Task + class Link

---
+++ post.rb
@@ -0,0 +1,29 @@
+class Post
+  def initialize
+    @created_at = Time.now
+    @text = nill
+  end
+
+  def read_from_console
+    #to do
+  end
+
+  def to_string
+    #to do
+  end
+
+  def save
+    file = File.new(file_path,"w:UTF-8")
+
+    for item in to_string do
+      file.puts(item)
+    end
+    file.close
+  end
+
+  def file_path
+    current_path = File.dirname(__FILE__)
+    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%s.txt")
+    return current_path + "/" + file_name
+  end
+end
---
+++ task.rb
@@ -0,0 +1,16 @@
+class Task < Post
+
+  def initialize
+    super
+    @due_data = Time.now
+  end
+
+  def read_from_console
+
+  end
+
+  def to_string
+
+  end
+
+end
